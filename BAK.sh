2790a56858b279d350fd01ca67c86ddf41d22690
Tristan Andersen <tristanpn02@gmail.com>
2022-03-09 03:15:02 +0000

    Remove working environment script

--- BAK.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash/
-cd /home/apherfox/Coding/OpenAI-Discord-Bot/.git/lost-found/other/
-FILES=*
-COUNTER = 0
-for f in $FILES do echo "Processing $f file..." git show $f > "/home/apherfox/Coding/OpenAI-Discord-Bot/$COUNTER.m" let COUNTER=COUNTER+1 done
